9432dd10ac0a3d05e3849e26c0e11ed224f6827f
Tino Mager <tino.mager@outlook.de>
2018-01-05 12:37:33 +0000

    added DHT22 code and modules

--- DHTServer.js
+++ DHTServer.js
@@ -1,12 +1,24 @@
 const express = require('express')
+var sensor = require('node-dht-sensor')
 var config = require('./config')
 
 const app = express()
 const port = config.webserver_port
 
 app.get('/', (request, response) => {
-    //Todo: Add DHT code here
-  response.send('Hello from DHT Server!')
+  sensor.read(22, 17, function(err, temp, hum)
+	{
+		var message = '';
+		if(!err){
+			message = '{ temp: '+ temp.toFixed(4) +', hum: '+ hum.toFixed(4) +' }';
+			//console.log('temp: ' + temp.toFixed(2) + '| hum: ' + hum.toFixed(2));
+		}
+		else{
+			//console.log('error reading values');
+			message = '{ error: ' + err + ' }';
+		}
+		response.send(message);
+	});
 })
 
 app.listen(port, (err) => {
