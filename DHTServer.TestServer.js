710a95bdd89e3dceee869594c275e296407e8dc3
Tino Mager <tino.mager@outlook.de>
2018-02-26 14:36:38 +0100

    changed testdata

--- DHTServer.TestServer.js
+++ DHTServer.TestServer.js
@@ -6,7 +6,7 @@ const app = express()
 const port = config.webserver_port
 var counter = 0;
 var tempValues = [17.0, 17.1, 17.0, 30.0, 17.1, 16.5, 8.0, 16.0, 17.1, 18.0];
-var humValues = [17.0, 17.1, 17.0, 30.0, 17.1, 16.5, 8.0, 16.0, 17.1, 18.0];
+var humValues = [67.0, 67.1, 67.0, 30.0, 67.1, 66.5, 98.0, 66.0, 67.1, 68.0];
 
 app.get('/', (request, response) => {
 		if(counter == tempValues.length){
